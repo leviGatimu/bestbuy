b5befefd3b9d60e49c4b996e5028e35076c3724a
Levi Gatimu <getmorelev@gmail.com>
2026-03-01 01:48:18 +0200

    Updates: 2026-02-28T23:48:16.310Z

--- models/user.js
+++ models/user.js
@@ -4,7 +4,17 @@ const userSchema = new mongoose.Schema ({
     name : {
         type : String
     },
+    age : {
+        type : Number
+    },
     email : {
         type : String
+    },
+    password : {
+        type : Number
     }
-})
+},{
+    timestamps: true
+});
+
+const User = mongoose.model("User", userSchema);
