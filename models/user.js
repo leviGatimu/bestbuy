1f540ac536474f7798a2dd1694aa41dc32e81e1e
Levi Gatimu <getmorelev@gmail.com>
2026-02-28 13:29:00 +0200

    Updates: 2026-02-28T11:28:57.966Z

--- models/user.js
+++ models/user.js
@@ -1,23 +1,21 @@
 import mongoose from "mongoose";
 
-const userschema = new mongoose.Schema({
-    name: {
-        type: String,
-    },
-    age: {
-        type: Number,
-    },
-    email: {
-        type: String,
-        unique: true
-    },
+const userSchema= new mongoose.schema({
+    name : {
+        type : String
+    }, 
     password: {
-        type: String,
+        type : Number, 
+    },
+    email : {
+        type : String,
+        unique : true
+    }, 
+    password: {
+        type : Number
     }
-}, { 
-    timestamps: true
-}); 
+});
 
-const User = mongoose.model("User", userschema);
+const user = mongoose.schema("user", userSchema);
 
-export default User;
+export default user;
