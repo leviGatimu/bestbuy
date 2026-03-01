818b60dd1c5860e96f4b47f1732e000cc6a87480
Levi Gatimu <getmorelev@gmail.com>
2026-03-01 13:45:39 +0200

    Updates: 2026-03-01T11:45:37.529Z

--- config/db.mjs
+++ config/db.mjs
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async (req, res) => {
   try{
     await mongoose.connect("mongodb://localhost:27017/bestbuy");
-    console.log("Mongoose succefully running ✅👏");
+    console.log("Mongoose succesfully running");
   }catch(err){
     console.log(err);
     process.exit(1);
--- index.mjs
+++ index.mjs
@@ -2,18 +2,19 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import userRoutes from './routes/users.mjs';
 import connectDB from './config/db.mjs';
+import { userData } from 'three/tsl';
 
 connectDB();
 
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.json());
 app.use('/users', userRoutes);
+app.use(bodyParser.json());
 
 app.get('/', (req, res)=> {
-  res.send("Welcome to homepage");
-  res.send("Please type '/users' in the url bar to continue!");
+  res.send("Hello from homepage");
 });
 
-app.listen(PORT , (req, res) => console.log(`Server succesfully running on : http://localhost:${PORT}`));
+app.listen(PORT , (req, res) => console.log(`Server succesfully running on port: http://localhost:${PORT}`));
+
