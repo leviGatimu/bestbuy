c907ea8b6f9c77ff42c985eb613df988f670496b
Levi Gatimu <getmorelev@gmail.com>
2026-03-01 23:45:50 +0200

    Updates: 2026-03-01T21:45:47.867Z

--- routes/products.js
+++ routes/products.js
@@ -1,19 +1,17 @@
+import { upload } from "../config/multer.js"; 
 import {
     getAllProducts,
     getProductByID,
     createProduct,
-    updateProductByID,
+    updateProduct,
     deleteProduct
 } from '../controllers/productController.mjs';
-
 import express from 'express';
 
-const router = express.Router();
+const router = express.Router()
 
 router.get('/', getAllProducts);
-router.get('/:id',getProductByID);
+router.get('/:id', getProductByID);
 router.post('/', createProduct);
-router.put('/:id', updateProductByID);
+router.put('/:id', updateProduct);
 router.delete('/:id', deleteProduct);
-
-export default router;
