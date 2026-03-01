74a398cb102eacca1e20a24b75de7377f3ab5e8e
Levi Gatimu <getmorelev@gmail.com>
2026-03-01 23:03:02 +0200

    Updates: 2026-03-01T21:03:00.616Z

--- config/db.mjs
+++ config/db.mjs
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try{
-    await mongoose.connect("mongodb://localhost:27017/bestbuy");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Mongoose is running succesfully ✅");
   }catch(err){
     console.log(err);
