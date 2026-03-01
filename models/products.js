6b3c24bb847e909a51c393d0518863c8ab66c24c
Levi Gatimu <getmorelev@gmail.com>
2026-03-01 14:56:36 +0200

    Updates: 2026-03-01T12:56:33.845Z

---
+++ models/products.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema ({
+    name : {
+        type : String,
+        required : true, 
+        trim : true
+    },
+    description : {
+        type : String,
+        default : ""
+   },
+   price : {
+    type : Number , 
+    required: true, 
+    min : 0, 
+   },
+   catgeory : {
+    type : String,
+    required: true
+   },
+   stock : {
+    type : Number,
+    default : 0
+   },
+    image : {
+    type : String,
+    default : ""
+    }
+});
+
+const Product = mongoose.model("Products", productSchema);
