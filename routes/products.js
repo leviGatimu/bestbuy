44b7cd9a6a9acced4d6bbd2cc2484f8f9d2df707
Levi Gatimu <getmorelev@gmail.com>
2026-03-01 15:13:17 +0200

    Updates: 2026-03-01T13:13:15.172Z

---
+++ routes/products.js
@@ -0,0 +1,19 @@
+import {
+    getAllProducts,
+    getProductByID,
+    createProduct,
+    updateProductByID,
+    deleteProduct
+} from '../controllers/productController';
+
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', getAllProducts);
+router.get('/:id',getProductByID);
+router.post('/', createProduct);
+router.put('/:id', updateProductByID);
+router.delete('/:id', deleteProduct);
+
+export default router;
