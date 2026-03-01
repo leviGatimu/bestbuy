0d8ddada9f8132bab30fa95dbebeb9be8c69de91
Levi Gatimu <getmorelev@gmail.com>
2026-03-01 02:03:20 +0200

    Updates: 2026-03-01T00:03:18.078Z

--- controllers/usercontroller.mjs
+++ controllers/usercontroller.mjs
@@ -2,19 +2,23 @@ import User from '../models/user.js';
 
 // GET all users
 export const getAllUsers = async (req, res) => {
-    
+    try{
+        const user = await User.find();
+        res.status(200).json(user);
+    }
+    catch(err){
+        res.status(500).json({error: err.message0});
+    }
 }
 
 // GET single user
 export const getUserById = async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id);
-        if (!user) return res.status(404).json({ error: 'User not found' });
+    try{
+        const user = await user.findById(req.params.id);
+        if(!user) return res.status(500).json({error: err.message});
         res.json(user);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
     }
-};
+}
 
 // POST create user
 export const createUser = async (req, res) => {
