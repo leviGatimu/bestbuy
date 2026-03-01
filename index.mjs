7f76f910a606fc9bb3927e75eb4eae76f8de7b5a
Levi Gatimu <getmorelev@gmail.com>
2026-03-01 21:14:20 +0200

    Updates: 2026-03-01T19:14:18.504Z

--- index.mjs
+++ index.mjs
@@ -1,14 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import connectDB from './config/db.mjs';
-import prouductRoutes from './routes/products.js';
+import userRoutes from './routes/users.mjs';
 
-connectDB();
 
 const app = express();
 const PORT = 3000;
 
-app.use('/products', prouductRoutes);
+app.use('/user', userRoutes);
 app.use(bodyParser.json());
 
 app.get('/', ()=>{
