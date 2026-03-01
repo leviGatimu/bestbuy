15834835dc222d8d933ca1e5a3754e9c7d8c59cf
Levi Gatimu <getmorelev@gmail.com>
2026-03-01 21:11:02 +0200

    Updates: 2026-03-01T19:11:00.319Z

--- index.mjs
+++ index.mjs
@@ -1,4 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import connectDB from './config/db.mjs';
-import prouductRoutes from './routes/products.js'
+import prouductRoutes from './routes/products.js';
+
+connectDB();
+
+const app = express();
+const PORT = 3000;
+
+app.use('/products', prouductRoutes);
+app.use(bodyParser.json());
+
+app.get('/', ()=>{
+  res.send("Hello from homepage");
+});
+
+app.listen(PORT , ()=> console.log(`Server succesfully running on port : http://localhost:${PORT}`));
