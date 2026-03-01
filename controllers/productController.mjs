af0a5c1a953c894d60d0465f7110958c94a2bb9a
Levi Gatimu <getmorelev@gmail.com>
2026-03-01 23:14:34 +0200

    Updates: 2026-03-01T21:14:32.339Z

--- controllers/productController.mjs
+++ controllers/productController.mjs
@@ -1,4 +1,5 @@
-import Product from '../models/products.js';
+import mongoose from 'mongoose';
+import Products from '../models/products.js';
 
 //1. Get all products
 //2. Get all products by ID
@@ -6,64 +7,9 @@ import Product from '../models/products.js';
 //4. Update a product by ID
 //5. Delete a product by ID
 
-//1. Get all products
 export const getAllProducts = async (req, res) => {
     try{
-        const product = await Product.find();
-        res.status(200).json(product);
-    }catch(err){
-        console.log(err);
-        res.status(500).json({error: err.message});
+        const user = await User.find
     }
 }
 
-//2. Get all products by ID
-export const getProductByID = async (req, res) => {
-    try{
-        const product = await Product.findById(req.params.id);
-        if(!product) return res.status(404).json({message : "Product was not found"});
-        res.status(200).json(product);
-    }catch(err){
-        console.log(err);
-        res.status(500).json({error: err.message});
-    }
-}
-
-//3. Create a product
-export const createProduct = async (req, res) => {
-    try{
-        const product = new Product(req.body);
-        await product.save();
-        res.status(201).json(product);
-        console.log(product);
-    }catch(err){
-        console.log(err);
-        res.status(500).json({error: err.message});
-    }
-}
-//4. Update a product by ID
-export const updateProductByID = async (req, res) => {
-    try{
-        const product = await Product.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            {new : true , runValidators: true});
-            if(!product) return res.status(404).json({message: "The product was not found."});
-            res.status(200).json(product);
-    }catch(err){
-        console.log(err);
-        res.status(500).json({error: err.message});
-    }
-}
-
-//5. Delete a product by ID
-export const deleteProduct = async (req, res) => {
-    try{
-        const product = await Product.findByIdAndDelete(req.params.id);
-        if(!product) return res.status(404).json({message: "The product wqas not found."});
-        res.status(200).json({message : "The product selected was deleted."});
-    }catch(err){
-        console.log(err);
-        res.status(500).json({error: err.message});
-    }
-}
