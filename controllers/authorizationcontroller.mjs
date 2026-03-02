6a4eebd26d669d6fa28134d735bf803e8b6b1be3
Levi Gatimu <getmorelev@gmail.com>
2026-03-02 19:02:16 +0200

    Updates: 2026-03-02T17:02:14.150Z

--- controllers/authorizationcontroller.mjs
+++ controllers/authorizationcontroller.mjs
@@ -11,7 +11,7 @@ export const register = async (req, res) => {
 
     const existing = await User.findOne({ email });
     if (existing) {
-      return res.status(409).json({ error: "User already exists. Please login." });
+     res.status(409).json({ error: "User already exists. Please login." });
     }
 
     const hashedPass = await bcrypt.hash(password, 10);
