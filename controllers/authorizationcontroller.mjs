93c795b1b23d83a94097f814c44013d238d0e1d1
Levi Gatimu <getmorelev@gmail.com>
2026-03-02 20:43:58 +0200

    Updates: 2026-03-02T18:43:55.817Z

--- controllers/authorizationcontroller.mjs
+++ controllers/authorizationcontroller.mjs
@@ -13,6 +13,11 @@ export const register = async (req, res) => {
         const user = await User.findOne({email});
         if(user) return res.json({error : "User already exists"});
         const hashedPass= await bcrypt.hash(password, 10);
+        const user = await User.create({
+            name, 
+            email, 
+            password: hashedPass
+        })l
         const token = jwt.sign(
             {id: user._id},
             process.env.JWT_SECRET,
