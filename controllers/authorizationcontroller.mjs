8349b32243b8247b5054cfae1cb9d0eecd4cf9de
Levi Gatimu <getmorelev@gmail.com>
2026-03-02 20:42:11 +0200

    Updates: 2026-03-02T18:42:09.144Z

--- controllers/authorizationcontroller.mjs
+++ controllers/authorizationcontroller.mjs
@@ -1,51 +1,41 @@
 import User from '../models/usermodel.js';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { use } from 'react';
 
 dotenv.config();
 
-export const register = async (req, res) => {
-    console.log("Hit register ✅");
+
+export const register = async (res, req) => {
     try{
         const {name , email , password} = req.body;
-        const existing = await User.findOne({email});
-        if(existing) return res.status(409).json({error: "User already exists."});
-        const hashedPass = await bcrypt.hash(password , 10);
-        const user = await User.create({
-            name, 
-            email, 
-            password: hashedPass
-        });
-      const token = jwt.sign(
-        {id: user._id},
-        process.env.JWT_SECRET,
-        {expiresIn : "7d"}
-      );
-      res.status(200).json({user , token});
-
-    }catch(err){
-        console.log(err);
-        res.status(500).json({err: err.message});
-    }
-}
-
-export const login = async(req,res) => {
-    console.log("HIT login ✅");
-    try{
-        const {email , password} = req.body;
         const user = await User.findOne({email});
-        if(!user) return res.status(409).json({error: "Invalid credentials. Please try again."});
-        const isMatch = await bcrypt.compare(password, user.password);
-        if(!isMatch) return res.status(404).json({error: "Invalid credentials. Please try again."});
-        
+        if(user) return res.json({error : "User already exists"});
+        const hashedPass= await bcrypt.hash(password, 10);
         const token = jwt.sign(
             {id: user._id},
             process.env.JWT_SECRET,
-            {expiresIn :"7d"}
+            {expiresIn : "7d"}
         );
-        res.json({user , token});
+        res.json({user, token});
     }catch(err){
-        res.status(500).json({error: "Internal sevrer error"});
+        res.status(404).json({error : err.message});
+    }
+}
+
+export const login = async(req, res) => {
+    try{
+        const {email , password} = req.body;
+        const user = await User.findOne({email});
+        if(!user) return res.json({error : "User does not exist"});
+        const isMatching = await bcrypt.compare(password, user.password);
+        const token = jwt.sign(
+            {id : user._id},
+            process.env.JWT_SECRET,
+            {expiresIn : "7d"});
+            res.json({user, token});
+    }catch(err){
+        res.status(500).json({error : err.message});
     }
 }
