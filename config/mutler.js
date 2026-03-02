659cf9358c355773133f87caf908cd8f9161a93c
Levi Gatimu <getmorelev@gmail.com>
2026-03-02 08:31:34 +0200

    Updates: 2026-03-02T06:31:32.538Z

--- config/mutler.js
+++ config/mutler.js
@@ -2,12 +2,13 @@ import multer from 'multer';
 import path from 'path';
 
 const storage = multer.diskStorage({
-  destination : (req, file , cb) => {
+  filename : (req, file , cb) => {
     cb(null , "uploads/");
   },
-  filename : (req, file, cb) => {
+  destination : (req, file, cb) => {
     cb(null , Date.now() + path.extname(file.originalname));
   }
 });
 
-export const upload = multer({stroage});
+
+export const upload = multer({storage});
