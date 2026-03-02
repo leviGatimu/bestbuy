6a5c11201ab2094b015440573a601950eeeadd9c
Levi Gatimu <getmorelev@gmail.com>
2026-03-02 14:08:58 +0200

    Updates: 2026-03-02T12:08:56.420Z

--- controllers/authorizationcontroller.mjs
+++ controllers/authorizationcontroller.mjs
@@ -1,23 +1,24 @@
 import User from '../models/user.js';
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
-import jwt from 'jsonwebtoken';
 
 dotenv.config();
 
-export const register = async (req, res) => {
+export const register = async(req, res) => {
     try{
         const {name , email , password} = req.body;
-        const existing = await User.findOne({email});
-        if(existing) return res.status(404).json({erorr : "User already exists"});
-        const hashedPass = bcrypt.hash(password,10);
-        const createdUser = await User.create({
-            name,
-            email,
+        const existing = await User.findOne();
+        if(existing) return res.status(404).json({error: "Credentials already exist. Please try again."});
+        const hashedPass= bcrypt.hash(password, 10);
+        const createdUser= await User.create({
+            name, 
+            email, 
             password: hashedPass,
         });
+        const token = jwt.token({id: createdUser.id}, process.env.jwt_secret || "Secret code", {expireIn : "1d"});
     }catch(err){
         console.log(err);
-        res.json(500).json({error: err.message});
+        res.status(500).json({error : err.message});
     }
 }
