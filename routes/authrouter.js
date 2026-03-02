5d486db08586b4a3c28af1966e271b1a8fe577a8
Levi Gatimu <getmorelev@gmail.com>
2026-03-02 19:07:27 +0200

    Updates: 2026-03-02T17:07:24.777Z

--- routes/authrouter.js
+++ routes/authrouter.js
@@ -6,7 +6,7 @@ import express from 'express';
 
 const router = express();
 
-router.post('/', register);
-router.post('/', login);
+router.post('/register', register);
+router.post('/login', login);
 
 export default router;
