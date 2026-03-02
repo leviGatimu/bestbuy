3eb626a46f7abb3eb64b46347f39a45356789e02
Levi Gatimu <getmorelev@gmail.com>
2026-03-02 14:18:36 +0200

    Updates: 2026-03-02T12:18:33.880Z

--- controllers/authorizationcontroller.mjs
+++ controllers/authorizationcontroller.mjs
@@ -16,7 +16,8 @@ export const register = async(req, res) => {
             email, 
             password: hashedPass,
         });
-        const token = jwt.token({id: createdUser.id}, process.env.jwt_secret || "Secret code", {expireIn : "1d"});
+        const token = jwt.sign({id: createdUser.id}, process.env.jwt_secret || "Secret code", {expireIn : "1d"});
+        res.status(201).json({user: createdUser, token});
     }catch(err){
         console.log(err);
         res.status(500).json({error : err.message});
@@ -25,6 +26,11 @@ export const register = async(req, res) => {
 export const login = async (req, res) => {
     try{
         const {email , password} = req.body;
-
+        const existing = await User.findOne();
+        if(!existing) return res.status(404).json({error: "Invalid credentials"});
+        const isPasMatch = bcrypt.compare(password, existing.password);
+        if(!isPasMatch) return res.status(404).json({error: "Invalid credentials"});
+        const token = jwt.sign({id: createdUser.id}, process.env.jwt_secret || "Secret code", {expireIn : "1d"});
+        res.status(200).json({user: createdUser, token});
     }
 }
